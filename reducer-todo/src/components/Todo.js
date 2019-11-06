321ae7882aa78dde8d1e2483c34289b3a52505e1
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-11-05 19:43:42 -0800

    TOGGLE_COMPLETED styling completed

--- reducer-todo/src/components/Todo.js
+++ reducer-todo/src/components/Todo.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { TOGGLE_COMPLETED } from '../actions'
+import React from "react";
+import { TOGGLE_COMPLETED } from "../actions";
 
 const Todo = props => {
-    //console.log(props);
+  //console.log(props);
 
-    const toggleCompleted = id => {
-props.dispatch({ type: TOGGLE_COMPLETED, payload: props.id})
-    };
+  const toggleCompleted = id => {
+    props.dispatch({ type: TOGGLE_COMPLETED, payload: props.id });
+  };
 
-    return(
-        <li onClick={toggleCompleted}>
-    {props.item}
-        </li>
-    )
+  return <li 
+  onClick={toggleCompleted}
+  className={`${props.completed ? "completed" : ""}`}
+  >{props.item}</li>;
 };
 
 export default Todo;
